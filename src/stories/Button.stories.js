8ecf2a57cdd800978e0d5ba6fb6a3a84692cad08
jo-duchan <jo_duchan@icloud.com>
2022-12-22 00:09:43 +0900

    Button DefaultValue Setting, argTypes Setting

--- src/stories/Button.stories.js
+++ src/stories/Button.stories.js
@@ -4,11 +4,6 @@ import Button from "components/Button";
 export default {
   component: Button,
   title: "Button",
-  btnType: "PRIMARY",
-  states: "DEFAULT",
-  size: "MEDIUM",
-  fixedWidth: false,
-  label: "Button",
 };
 
 function StoryBook({ ...args }) {
@@ -22,21 +17,18 @@ function StoryBook({ ...args }) {
 export const Default = StoryBook.bind({});
 
 Default.args = {
-  // btnType: "PRIMARY",
-  // states: "DEFAULT",
-  // size: "MEDIUM",
-  // fixedWidth: false,
-  // label: "Button",
+  btnType: "PRIMARY",
+  size: "MEDIUM",
+  fixedWidth: false,
+  label: "Button",
 };
 
-// export const Secondary = StoryBook.bind({});
-
-// Secondary.args = {
-//   btnType: "SECONDARY",
-//   states: "DEFAULT",
-//   size: "MEDIUM",
-//   fixedWidth: false,
-//   label: "Button",
-// };
+Default.argTypes = {
+  states: {
+    defaultValue: "DEFAULT",
+    options: ["DEFAULT", "FOCUSED", "LOADING", "DISABLED"],
+    control: "radio",
+  },
+};
 
 const Container = styled.div``;
