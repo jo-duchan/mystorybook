657135245322294981e4380a3db02e3dc384de4e
jo-duchan <jo_duchan@icloud.com>
2023-01-18 00:12:00 +0900

    button 리턴 값 표시

--- src/stories/Button.stories.js
+++ src/stories/Button.stories.js
@@ -1,12 +1,30 @@
+import React, { useState } from "react";
+import styled from "styled-components";
 import Button from "components/Button";
+
+// Style
+import ColorSystem from "styles/color-system";
+import { Text } from "styles/typography";
 
 export default {
   component: Button,
   title: "Button",
 };
 
-function StoryBook({ ...args }) {
-  return <Button {...args} />;
+function StoryBook({ onClick, ...args }) {
+  const [count, setCount] = useState(0);
+
+  const handleClick = (ev) => {
+    ev.preventDefault();
+    setCount((current) => (current = current + 1));
+  };
+
+  return (
+    <Container>
+      <Label>Count: {count}</Label>
+      <Button onClick={handleClick} {...args} />
+    </Container>
+  );
 }
 
 export const Default = StoryBook.bind({});
@@ -30,3 +48,14 @@ Default.argTypes = {
     control: "radio",
   },
 };
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 30px;
+`;
+
+const Label = styled.span`
+  ${Text.Body500};
+  color: ${ColorSystem.Neutral[800]};
+`;
