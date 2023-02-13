63828e3035c20a4490e4c19918c64e8ec084e5db
jo-duchan <jo_duchan@icloud.com>
2023-02-13 18:22:41 +0900

    ColorSystem 작업중

--- src/stories/ColorSystem.stories.js
+++ src/stories/ColorSystem.stories.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useCallback } from "react";
 import styled from "styled-components";
 
 // Style
@@ -6,12 +6,31 @@ import ColorSystem from "styles/color-system";
 import { Text } from "styles/typography";
 
 export default {
-  // component:
   title: "Core/Color System",
 };
 
 function StoryBook() {
-  return <Container> </Container>;
+  const colors = Object.keys(ColorSystem);
+  const drawItem = useCallback((x, i) => {
+    const result = [];
+    const options = ColorSystem[x];
+    const keys = Object.keys(options);
+
+    keys.forEach((x, i) => {
+      const key = keys[i];
+      const value = options[key];
+
+      result.push(<Item key={i}>{`${value} ${key}`}</Item>);
+    });
+    return result;
+  }, []);
+  return (
+    <Container>
+      {colors.map((x, i) => (
+        <List key={i}>{drawItem(x, i)}</List>
+      ))}
+    </Container>
+  );
 }
 
 export const Default = StoryBook.bind({});
@@ -19,3 +38,7 @@ export const Default = StoryBook.bind({});
 Default.args = {};
 
 const Container = styled.div``;
+
+const List = styled.div``;
+
+const Item = styled.div``;
